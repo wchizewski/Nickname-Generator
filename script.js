0feefc9326de2acd0ab87e8859f8623fe5a114b4
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-13 14:48:38 -0600

    almost done

---
+++ script.js
@@ -0,0 +1,25 @@
+let randomBtn = document.getElementById("random")
+let allBtn = document.getElementById("all")
+
+randomBtn.addEventListener("click", random)
+allBtn.addEventListener("click", all)
+let firstName = document.getElementById("firstName")
+let lastName = document.getElementById("lastName")
+let nickname = document.getElementById("nickname")
+
+
+let nicknameArray = [' "Crusher" ', ' "the Scientist" ', ' "Twinkle-toes" ', ' "the Coder" ', ' "the Jester" ', ' "the Sloth" ', ' "Quick-Silver" ']
+
+function random() {
+    let randomNickname = Math.round(Math.random() * 6)
+    console.log(randomNickname)
+    document.getElementById("nickname").innerHTML = firstName.value + nicknameArray[randomNickname] + lastName.value;
+}
+
+function all() {
+    let nicknames;
+    for(let i = 0; i < 6; i++){
+        nicknames = firstName.value + nicknameArray[i] + lastName.value;
+    }
+    nickname.innerHTML = nicknames
+}
